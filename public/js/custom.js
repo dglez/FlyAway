6b0568fda492fd4f8e849d332c19152b3aa6c3aa
Daniel\ Glez <dgonz325@fiu.edu>
2016-11-15 16:32:10 -0500

    input last update ready for production

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,18 @@
 
-var input = $('#from-airport');
 
-input.keyup(function () {
 
-    if (input.val().length === 3){
-        input.attr('list','places');
-    }else if (input.val().length < 3 && input.attr('list') == "places"){
-        input.attr('list', '');
-    }
+var input = $('#from-airport, #to-airport');
 
+input.each(function () {
+    var element = $(this);
+    element.on('input', function () {
+
+        if (element.val().length === 3) {
+            element.attr('list', 'places');
+        } else if (element.val().length < 4 && element.attr('list') == "places") {
+            element.attr('list', '');
+        }
+    });
 });
+
 
